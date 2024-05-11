89c8225c0d5c997933cebd011b6bb2548e051f1b
Yohans Quezada <yohansquezada@gmail.com>
2024-05-11 18:15:07 -0400

    estelas + warrior

---
+++ Warrior.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+class_name Warrior
+
+
+var speed = 150
+var acceleration = 300
+var noise_signal = 0
+
+
+@export var animation_scene: PackedScene
+
+
+@onready var player = $"."
+
+
+func _physics_process(delta: float)-> void:
+	
+	if is_multiplayer_authority():
+		var move_input_x = Input.get_axis("move_left","move_right")
+		var move_input_y = Input.get_axis("move_up","move_down")
+		velocity.x = move_toward(velocity.x, move_input_x * speed, acceleration * delta)
+		velocity.y = move_toward(velocity.y, move_input_y * speed, acceleration * delta)
+		send_data.rpc(global_position)
+	move_and_slide()
+	
+func _input(event: InputEvent)-> void:
+	if is_multiplayer_authority():
+		if event.is_action_pressed("action"):
+			var echo_visu = animation_scene.instantiate()
+			add_child(echo_visu)
+			noise.rpc()
+
+func setup(player_data: Statics.PlayerData):
+	name = str(player_data.id)
+	set_multiplayer_authority(player_data.id)
+	
+
+@rpc("authority","call_local","reliable")
+func noise()-> void:
+	noise_signal = 1
+	
+@rpc("authority","call_local","reliable")
+func action():
+	pass
+
+@rpc("authority", "call_local","reliable")
+func send_data(pos: Vector2):
+	global_position = pos
+	
+	
+	
--- scenes/echo_fragmento.gd
+++ scenes/echo_fragmento.gd
@@ -3,14 +3,17 @@ extends Area2D
 @onready var sprite_2d = $Sprite2D
 
 var speed = 250
-const lifetime = 5
+const lifetime = 2
 var tiempo = 0
 var collision_cnt = 0
 var target_angle = 0
 var wall_touch = 0
 
+var current_trail: Trail
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	make_trail()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -30,10 +33,18 @@ func _on_body_entered(body: Node2D):
 	if collision_cnt==2 and body is CharacterBody2D:
 		#Debug.log("Tocó un jugador")
 		sprite_2d.modulate = Color.RED	
+		current_trail.modulate = Color.RED
 	elif collision_cnt>1:
 		wall_touch = 1
 		#Debug.log("Tocó una pared")
 		
+		
+func make_trail() -> void:
+	if current_trail:
+		current_trail.stop()
+	current_trail = Trail.create()
+	#current_trail.modulate = Color.RED
+	add_child(current_trail)
 	#if collision_cnt>1:
 		#Debug.log(speed * transform.x)
 		
